61ac7b06f61118ca20eeea78529f1a766d925d80
Agwenchez <agweraenock76@gmail.com>
2019-07-31 11:27:04 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,97 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
+//support parsing of application/x-www-form-urlencoded post data
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// schema
+const contactSchema = new mongoose.Schema({
+	name: String,
+	telephone_number: String
+});
+
+const ContactsModel = mongoose.model('ContactModel', contactSchema);
+
+
+app.post(`/create-contacts`, (req, res) => {
+	const data = new ContactsModel(req.body);
+	console.log(req.body);
+	data.save().then((result) => {
+		res.status(200).json(result);
+	}).catch((err) => {
+		res.status(500).send({ message: 'could not save the data into the database' })
+	});
+})
+
+app.get('/contacts', (req, res) => {
+	ContactsModel.find().then((result) => {
+
+		res.json(result);
+	}).catch((err) => {
+		res.json(err.stack);
+	});
+
+})
+
+app.post('/send_sms', (req, res) => {
+	const data = req.body;
+	// console.log(data);
+
+	ContactsModel.find({ telephone_number: data.telephone_number }).then((result) => {
+		// console.log(result);
+		var num = result; //undefined
+		console.log(num)
+		let tel = [];
+		num.map(item => {
+			tel.push(item.telephone_number);
+		})
+		// console.log(tel);
+		const options = {
+			username: 'your username',
+			apiKey: 'your API key'
+		};
+
+		// initialize africastalking gateway
+		const africastalking = require('africastalking')(options);
+
+		// sms object of africastalking package
+		const sms = africastalking.SMS;
+
+		// sending parameters
+		const sending_options = {
+			to: tel,
+			message: 'Cheers developers, lets keep with the good work!!!',
+			from: 'LakeHub'
+		};
+
+		// send sms
+		sms.send(sending_options)
+			.then(response => {
+				console.log(response);
+				res.send(response);
+			})
+			.catch(error => {
+				console.log(error);
+				res.send(error);
+			});
+	}).catch((err) => {
+		res.status(500).send({ message: `the number does not exist ${err}` })
+		console.log(err);
+	});
+
+
+});
+mongoose
+	.connect('mongodb://localhost:27017/sms-app', { useNewUrlParser: true })
+	.then(() => {
+		app.listen(port, () => console.log(`Academy app running on port ${port}`));
+	})
+	.catch(error => {
+		console.log({
+			message: `Unable to establish a connection to the server ${error}`
+		});
+	});
